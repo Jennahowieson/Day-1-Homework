aa860fd68e1c73b39ebdd54067099a5ab928a1fe
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-10-17 17:27:39 +0100

    bug fixes

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,10 +1,15 @@
-name = jenna
+name = "jenna"
 age = 30
-nationality = Scottish 
+nationality = "Scottish" 
 
 def first_code():
-    print("Hello my name is " name " and I am " age "and I am " nationality)
+    print("Hello my name is " + name + " and I am " + str(age)  + " and I am " + nationality)
 first_code()
+
+
+
+
+
 
 
 
